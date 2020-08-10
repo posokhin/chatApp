f4ba11f91db702050ebb78921920b48a349afa17
Posohin Anton <posohin7@gmail.com>
2020-08-10 20:50:16 +0300

    hot fix

--- plugins/socket.js
+++ plugins/socket.js
@@ -4,7 +4,7 @@ import VueSocketIO from 'vue-socket.io'
 export default function ({ store }) {
   Vue.use(new VueSocketIO({
     debug: false,
-    connection: 'http://localhost:3000',
+    connection: `${location.hostname}:3000`,
     vuex: {
       store,
       actionPrefix: 'SOCKET_',
